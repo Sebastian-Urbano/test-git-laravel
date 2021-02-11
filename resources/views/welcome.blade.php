4aa87e7e405d624b989f1f34cb14aec968bd911a
Sebastian Urbano <saurbadev@gmail.com>
2021-02-11 18:24:04 -0300

    primer commit en rama nueva

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hola Mundo
+                    Hola Mundo !!!!!
                 </div>
 
                 <div class="links">
